f1ec6b9c4f7c18e877cbc5e696b6dffe4242bb3a
devmarrie <annemarrieakeyo42@gmail.com>
2023-02-17 15:15:35 +0300

    removing the session and user import

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """User Class"""
 from sqlalchemy import Column, String
-from base_model import BaseModel,session
+from base_model import BaseModel
 from sqlalchemy.orm import relationship
 
 class User(BaseModel):
@@ -9,9 +9,7 @@ class User(BaseModel):
     name = Column(String(60), nullable=False)
     email = Column(String(128), nullable=False)
 
-    chats = relationship('Chat', back_populates='user')
-    questions = relationship("Question", back_populates='user')
+    chats = relationship("Chat", back_populates="user")
+    questions = relationship("Question", back_populates="user")
     
-    def get_user_by_id(user_id):
-        return session.query(User).filter_by(id=user_id).first()
-
+    
