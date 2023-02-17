b8a7f557c739b9160b55cf2f3bbecaf191ee66c8
devmarrie <annemarrieakeyo42@gmail.com>
2023-02-17 15:14:56 +0300

    Removing user-response relationship

--- backend/models/response.py
+++ backend/models/response.py
@@ -2,19 +2,14 @@
 """Stores the response sent to users"""
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from base_model import BaseModel,session
+from base_model import BaseModel
 
 class Response(BaseModel):
     __tablename__ = "responses"
     answer = Column(String(800), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'))
     chat_id = Column(String(60), ForeignKey('chats.id'))
 
-    chat = relationship("Chat", back_populates='responses')
-    questions = relationship("Question", back_populates='response')
-    user = relationship("User", back_populates='responses')
+    chat = relationship("Chat", back_populates="responses")
+    questions = relationship("Question", back_populates="response")
+    
 
-
-
-    def get_response_by_id(response_id):
-        return session.query(Response).filter_by(id=response_id).first()
