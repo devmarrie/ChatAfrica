b4d046f3db56610d7a666a5614e1f2a99e003830
devmarrie <annemarrieakeyo42@gmail.com>
2023-02-28 17:10:25 +0300

    relationships configuration

--- backend/models/user.py
+++ backend/models/user.py
@@ -3,11 +3,9 @@ from base_model import BaseModel
 
 
 class User(BaseModel):
-    __tablename__ = 'users'
-    
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-
+    chats = db.relationship('Chat', backref='user')
 
     
     
