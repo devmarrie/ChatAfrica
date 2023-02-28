061dae1f71dd7976ceea2a8c0e4e63d83528d1ac
Uchechukwu Ottah <coderboy.exe@gmail.com>
2023-02-28 12:20:04 +0100

    Update relationships

--- backend/__init__.py
+++ backend/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DB_NAME}' # To be reconfigured with mysql
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # To be reconfigured with mysql
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
