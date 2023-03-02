f65bb91100e68c7a77a13756de0dd2a8a87afa51
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-02 12:17:03 +0300

    Converting the colums to a dictonary

--- backend/models/base_model.py
+++ backend/models/base_model.py
@@ -1,8 +1,13 @@
-from app import db
+import uuid
+from database import db
 from datetime import datetime
 
 class BaseModel(db.Model):
     __abstract__ = True
-    id = db.Column(db.String(60), primary_key=True)
+    id = db.Column(db.String(60), primary_key=True, default=str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
