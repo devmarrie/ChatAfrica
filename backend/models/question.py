1238e6864da62a05672ec5ba1848a67652ba5910
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-02 12:18:50 +0300

    Linking the response to the question

--- backend/models/question.py
+++ backend/models/question.py
@@ -1,8 +1,10 @@
-from app import db
+from database import db
 from base_model import BaseModel
 
 
 class Question(BaseModel):
     que = db.Column(db.Text, nullable=False)
-    chat_id = db.Column(db.String(60), db.ForeignKey('chat.id'))
+    res_id = db.Column(db.String(60), db.ForeignKey('response.id'))
+   
+    
     
