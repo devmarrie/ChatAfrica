6a6af2cf64888fbb8cd05f1d21ed5226ca06f3b4
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-02 12:19:12 +0300

    Linking questions to one response

--- backend/models/response.py
+++ backend/models/response.py
@@ -1,9 +1,11 @@
-from app import db
+from database import db
 from base_model import BaseModel
 
 class Response(BaseModel):
     content = db.Column(db.Text, nullable=False)
     chat_id = db.Column(db.String(60), db.ForeignKey('chat.id'), nullable=False)
+    questions = db.relationship('Question', backref='response')
+    
 
     
     
